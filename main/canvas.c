d6dc88e36da968b858b1ce0ea81edd1625957934
Alexander Baransky <alexander.baranskiy@yandex.ru>
2025-06-20 00:38:07 +0300

    canvas: Fix draw_line functions

--- main/canvas.c
+++ main/canvas.c
@@ -44,14 +44,14 @@ void cv_set_pixel(struct canvas *cv, uint8_t x, uint8_t y, crgb color)
 
 void cv_draw_line_v(struct canvas *cv, uint8_t x, uint8_t y1, uint8_t y2, crgb color)
 {
-    for (uint8_t i = y1 * cv->width; i < y2 * cv->width; i += cv->width)
+    for (uint8_t i = y1 * cv->width; i <= y2 * cv->width; i += cv->width)
         cv->buf[i + x] = color;
 }
 
 void cv_draw_line_h(struct canvas *cv, uint8_t x1, uint8_t x2, uint8_t y, crgb color)
 {
     uint8_t pos = y * cv->width;
-    for (uint8_t j = x1; j < x2; j++)
+    for (uint8_t j = x1; j <= x2; j++)
         cv->buf[pos + j] = color;
 }
 
