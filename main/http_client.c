c5ae7cd8536c5884dbcdb6c32135a4a8d292aef1
Alexander Baransky <alexander.baranskiy@yandex.ru>
2025-06-20 00:37:50 +0300

    http_client: (almost) total rewrite
    Use simple and straightforward function instead of event handler, now it supports chunked encoding.
    Allow to set size for buffer (if response is chunked)

--- main/http_client.c
+++ main/http_client.c
@@ -13,121 +13,107 @@
 #include "http_client.h"
 
 #define HTTP_RETRY_COUNT    3
+#define HTTP_DEFAULT_CHUNKED_BUFFER_SIZE    4096
 
 static const char *TAG = "http_client";
 
-static esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+static int do_request(const char *url, esp_http_client_method_t type, struct http_header *headers, const char *body, char **buffer, size_t chunked_buf_size)
 {
-    static int bytes_read;
-    switch(evt->event_id) {
-        case HTTP_EVENT_ERROR:
-            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
-            break;
-        
-        case HTTP_EVENT_ON_DATA:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA");
-            if (evt->user_data == NULL) {
-                ESP_LOGE(TAG, "Output buffer must be specified!");
-                return ESP_FAIL;
-            }
-
-            // Ignore chunked responses
-            if (esp_http_client_is_chunked_response(evt->client))
-                break;
-
-            char **buffer = (char **) evt->user_data;
-            size_t content_len = esp_http_client_get_content_length(evt->client);
-
-            // Clean the buffer in case of a new request
-            if (bytes_read == 0) {
-                *buffer = pvPortMalloc(content_len + 1);
-                if (*buffer == NULL) {
-                    ESP_LOGE(TAG, "Failed to allocate memory for an output buffer");
-                    return ESP_FAIL;
-                }
-                memset(*buffer, 0, content_len + 1);
-            }
-
-            size_t copy_len = MIN(evt->data_len, content_len - bytes_read);
-            if (copy_len) {
-                memcpy(*buffer + bytes_read, evt->data, copy_len);
-            }
-            bytes_read += copy_len;
-            break;
-        
-        case HTTP_EVENT_ON_FINISH:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-            bytes_read = 0;
-            break;
-        
-        case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
-            int mbedtls_err = 0;
-            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
-            if (err != 0) {
-                ESP_LOGW(TAG, "Last esp error code: 0x%x", err);
-                ESP_LOGW(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
-            }
-            bytes_read = 0;
-            break;
-        
-        default:
-            break;
-    }
-    return ESP_OK;
-}
-
-static esp_http_client_handle_t prepare_http_client(const char *url, struct http_header *headers, char **response)
-{
-    *response = NULL;
     esp_http_client_config_t config = {
         .url = url,
-        .event_handler = _http_event_handler,
-        .user_data = response,
+        .method = type,
 #if CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
         .crt_bundle_attach = esp_crt_bundle_attach,
 #endif
     };
     esp_http_client_handle_t client = esp_http_client_init(&config);
-
     if (headers != NULL) {
-        for (uint8_t i = 0; (void *)(headers[i].key) != NULL; i++) {
+        for (uint8_t i = 0; (void *)(headers[i].key) != NULL; i++)
             esp_http_client_set_header(client, headers[i].key, headers[i].value);
-        }
     }
-    return client;
-}
+    size_t write_len = 0;
+    if ((type == HTTP_METHOD_POST || type == HTTP_METHOD_PUT) && body != NULL)
+        write_len = strlen(body);
 
-static int try_request(esp_http_client_handle_t client, const char *type)
-{
     for (uint8_t i = 0; i < HTTP_RETRY_COUNT; i++) {
-        esp_err_t err = esp_http_client_perform(client);
-        if (err == ESP_OK) {
-            return esp_http_client_get_status_code(client);
-        } else {
-            ESP_LOGW(TAG, "HTTP %s request failed: %s", type, esp_err_to_name(err));
+        // Open connection
+        esp_err_t err;
+        if ((err = esp_http_client_open(client, write_len)) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+            continue;
         }
+        if (write_len > 0) {
+            if (esp_http_client_write(client, body, write_len) < 0) {
+                ESP_LOGE(TAG, "Failed to write body");
+                esp_http_client_close(client);
+                continue;
+            }
+        }
+        int content_len = esp_http_client_fetch_headers(client);
+        if (content_len < 0) {
+            ESP_LOGW(TAG, "Timeout or some other error while fetching headers");
+            esp_http_client_close(client);
+            continue;
+        }
+
+        // Buffer allocation
+        size_t buf_size;
+        if (!esp_http_client_is_chunked_response(client)) {
+            // Allocate buffer with Content-Length size
+            buf_size = content_len + 1;
+        } else {
+            // Allocate buffer with predefined size
+            buf_size = chunked_buf_size;
+        }
+        *buffer = pvPortMalloc(buf_size);
+        if (*buffer == NULL) {
+            ESP_LOGE(TAG, "Failed to allocate memory for an output buffer");
+            esp_http_client_close(client);
+            esp_http_client_cleanup(client);
+            return ESP_FAIL;
+        }
+        memset(*buffer, 0, buf_size);
+
+        // Read response
+        size_t read_len = 0;
+        while (read_len < buf_size) {
+            int chunk_len = esp_http_client_read(client, *buffer + read_len, buf_size - read_len);
+            if (chunk_len < 0) {
+                ESP_LOGW(TAG, "Timeout or some other error while fetching data");
+                esp_http_client_close(client);
+                continue;
+            }
+            if (chunk_len == 0)
+                break;
+            read_len += chunk_len;
+        }
+
+        int code = esp_http_client_get_status_code(client);
+        esp_http_client_close(client);
+        esp_http_client_cleanup(client);
+        return code;
     }
-    return -1;
+    return ESP_FAIL;
 }
 
 int http_get(const char *url, struct http_header *headers, char **response)
 {
-    esp_http_client_handle_t client = prepare_http_client(url, headers, response);
-    int ret = try_request(client, "GET");
-    esp_http_client_cleanup(client);
-    return ret;
+    return do_request(url, HTTP_METHOD_GET, headers, NULL, response, HTTP_DEFAULT_CHUNKED_BUFFER_SIZE);
+}
+
+int http_get_with_bufsize(const char *url, struct http_header *headers, char **response, size_t buf_size)
+{
+    return do_request(url, HTTP_METHOD_GET, headers, NULL, response, buf_size);
 }
 
 int http_post(const char *url, struct http_header *headers, const char *body, char **response)
 {
-    esp_http_client_handle_t client = prepare_http_client(url, headers, response);
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-    esp_http_client_set_post_field(client, body, strlen(body));
+    return do_request(url, HTTP_METHOD_POST, headers, body, response, HTTP_DEFAULT_CHUNKED_BUFFER_SIZE);
+}
 
-    int ret = try_request(client, "POST");
-    esp_http_client_cleanup(client);
-    return ret;
+int http_post_with_bufsize(const char *url, struct http_header *headers, const char *body, char **response, size_t buf_size)
+{
+    return do_request(url, HTTP_METHOD_POST, headers, body, response, buf_size);
 }
 
 int http_post_json(const char *url, const char *body, char **response)
--- main/include/http_client.h
+++ main/include/http_client.h
@@ -1,6 +1,8 @@
 #ifndef __HTTP_CLIENT_H__
 #define __HTTP_CLIENT_H__
 
+#include <stddef.h>
+
 struct http_header {
     const char *key;
     const char *value;
@@ -17,6 +19,9 @@ struct http_header {
  */
 int http_get(const char *url, struct http_header *headers, char **response);
 
+// Allows to change the default buffer size for chunked encoding
+int http_get_with_bufsize(const char *url, struct http_header *headers, char **response, size_t buf_size);
+
 /**
  * http_get() - Sends POST request
  *
@@ -28,6 +33,9 @@ int http_get(const char *url, struct http_header *headers, char **response);
  * @returns: HTTP response status code or -1 if error occured
  */
 int http_post(const char *url, struct http_header *headers, const char *body, char **response);
+
+// Allows to change the default buffer size for chunked encoding
+int http_post_with_bufsize(const char *url, struct http_header *headers, const char *body, char **response, size_t buf_size);
 
 // Simple wrapper to set a proper Content-Type
 int http_post_json(const char *url, const char *body, char **response);
